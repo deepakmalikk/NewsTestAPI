c4fa786dbb87101bf7d9e2be425e0e4ded7bffde
Deepak Malik <65271369+deepakmalikk@users.noreply.github.com>
2025-02-24 22:54:00 +0530

    country specific news

--- src/app.py
+++ src/app.py
@@ -1,8 +1,14 @@
-from newsdataapi import NewsDataAPIClient
+from newsdataapi import NewsDataApiClient
 import os 
 from dotenv import load_dotenv
 
 load_dotenv()
 api_key = os.getenv("API_KEY")
 #initialize API client
-news = NewsDataAPIClient(apikey=api_key)
+api = NewsDataApiClient(apikey=api_key)
+
+#To get latest news related to country india 
+response = api.latest_api(country="au,us")
+news_title = [i['title'] for i in response['results']]
+
+print(len(news_title))
