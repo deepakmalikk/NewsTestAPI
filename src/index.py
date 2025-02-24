56fb78096facdc7cad22ec729297063466076753
Deepak Malik <65271369+deepakmalikk@users.noreply.github.com>
2025-02-24 22:25:52 +0530

    API request using normal https request method

--- src/index.py
+++ src/index.py
@@ -1,19 +1,20 @@
-import requests
-import os
-from dotenv import load_dotenv
+# import requests
+# import os
+# from dotenv import load_dotenv
 
-load_dotenv()   
+# load_dotenv()   
 
-API_KEY = os.getenv("API_KEY")
+# API_KEY = os.getenv("API_KEY")
 
-if not API_KEY:
-    raise ValueError("Missing API key")
-else:
-    print("API Key found")
+# if not API_KEY:
+#     raise ValueError("Missing API key")
 
-url ="https://newsdata.io/api/1/latest?apikey={}".format(API_KEY)
+# baseURL = "https://newsdata.io/api/1/latest?apikey={}".format(API_KEY)
 
-response = requests.get(url)
-result= response.json()
-for i in result['results']:
-    print(i['title'])
+# #request to retrieve news articles related to football
+# url =baseURL+"&q=football"
+# response = requests.get(url)
+# result= response.json()
+# for i in result['results']:
+#     print(i['title'])
+
